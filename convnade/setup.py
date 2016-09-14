e3ec2700f28f32a46d11e519bb8541c9533b721a
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-09-14 10:44:31 -0400

    Reorganized folders

--- convnade/setup.py
+++ convnade/setup.py
@@ -4,15 +4,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ConvNADE',
-    version='0.0.1',
-    author='Marc-Alexandre Côté',
+    name='NADE',
+    version='1.0.0',
+    author='Benigno Uria, Marc-Alexandre Côté, Karol Gregor, Iain Murray, Hugo Larochelle',
     author_email='marc-alexandre.cote@usherbrooke.ca',
-    url='https://github.com/MarcCote/ConvNADE',
+    url='https://github.com/MarcCote/NADE',
     packages=find_packages(),
     license='LICENSE',
-    description='Convolutional extension for NADE.',
+    description='Neural Autoregressive Distribution Estimation.',
     long_description=open('README.md').read(),
-    install_requires=['smartlearner'],
+    install_requires=['theano', 'smartlearner'],
     dependency_links=['https://github.com/SMART-Lab/smartlearner/archive/master.zip#egg=smartlearner-0.0.1']
 )
